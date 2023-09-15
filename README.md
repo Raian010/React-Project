d5edfdbf99e3aade5c2e0ce4a5a7421218356cd1
Raian010 <md.abrarulhoque123@gmail.com>
2023-09-15 15:50:53 +0600

    remaining credit hours section and its conditioning

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
- ## Three Features of this project
-  
+## Three Features of this project
+-Credit hours adding and also there will be some time limits.
+-Course name adding on the right side of it.
+-Credit remaining hours section.  
--- src/components/CartAccounts/CartAccounts.jsx
+++ src/components/CartAccounts/CartAccounts.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import CartAccount from './CartAccount';
 const CartAccounts = ({selectCourses,totalCredit,remainingCredit}) => {
     return (
-        <div className="text-left border  ml-2 mt-6 p-4">
+        <div className="text-left border h-full rounded-lg  ml-2 mt-6 p-4">
             <h2 className="text-xl font-semibold text-blue-400 my-2">Credit hour Remaining: {remainingCredit} hr </h2>
             <hr className="my-2" />
             <h2 className="text-xl my-2 font-semibold">Course Name </h2>
